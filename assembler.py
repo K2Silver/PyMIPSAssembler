12981fe47b66933ed6ed6a2fbf002c423b0c673c
Kairi Kozuma <kairikozuma@gmail.com>
2016-02-29 14:14:06 -0500

    Create a single dictionary for all instructions

--- assembler.py
+++ assembler.py
@@ -3,25 +3,29 @@ import re
 from bitstring import BitArray
 import mips_table
 
+OPCODE = 0
+SHIFT_AMT = 4
+FUNCTION_CODE = 5
+
 def assemble(instruction):
     pattern_rtype = "(\w{2,3})\s*\$([3][0-1]|[1-2]\d|\d),\s*\$([3][0-1]|[1-2]\d|\d),\s*\$([3][0-1]|[1-2]\d|\d)"
     prog   = re.compile(pattern_rtype)
     result = prog.match(instruction)
-    r_instruction = result.group(1)
-    r1 = result.group(2)
-    r2 = result.group(3)
-    r3 = result.group(4)
+    instruction_name = result.group(1)
+    r0 = result.group(2)
+    r1 = result.group(3)
+    r2 = result.group(4)
 
-    function_code = mips_table.function_codes[r_instruction];
-    opcode = '0b000000';
+    instr_info = mips_table.instruction_info[instruction_name];
+    print instr_info
+
     bin_inst = BitArray('0x00000000')
-    bin_inst[0:6] = opcode
-    bin_inst[6:11] = int(r2)
-    bin_inst[11:16] = int(r3)
-    bin_inst[16:21] = int(r1)
-    bin_inst[21:26] = 0
-    bin_inst[26:32] = function_code
-    # .append(int(r2)).append(int(r3)).append(int(r1)).append('0b100000')
+    bin_inst[0:6] = instr_info[OPCODE]
+    bin_inst[6:11] = int(r1)
+    bin_inst[11:16] = int(r2)
+    bin_inst[16:21] = int(r0)
+    bin_inst[21:26] = instr_info[SHIFT_AMT]
+    bin_inst[26:32] = instr_info[FUNCTION_CODE]
     print bin_inst
 
 assemble("add $3, $2, $10")
