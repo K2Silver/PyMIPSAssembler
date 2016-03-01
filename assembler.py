7e5bf5c81980ddbca44dd79972ef86afaa17fe63
Kairi Kozuma <kairikozuma@gmail.com>
2016-03-01 11:57:46 -0500

    Use dispatch to replace bit values in binary instruction

--- assembler.py
+++ assembler.py
@@ -4,29 +4,51 @@ from bitstring import BitArray
 import mips_table
 
 OPCODE = 0
-SHIFT_AMT = 4
-FUNCTION_CODE = 5
+FUNCTION_CODE = 1
+INSTRUCTION_TYPE = 2
 
 def assemble(instruction):
-    pattern_rtype = "(\w{2,3})\s*\$([3][0-1]|[1-2]\d|\d),\s*\$([3][0-1]|[1-2]\d|\d),\s*\$([3][0-1]|[1-2]\d|\d)"
-    prog   = re.compile(pattern_rtype)
-    result = prog.match(instruction)
-    instruction_name = result.group(1)
-    r0 = result.group(2)
-    r1 = result.group(3)
-    r2 = result.group(4)
-
-    instr_info = mips_table.instruction_info[instruction_name];
-    print instr_info
+    regex_instr = re.compile("(\w{2,5})").match(instruction)
+    instruction_name = regex_instr.group(1)
+    instr_info = mips_table.instruction[instruction_name]
 
     bin_inst = BitArray('0x00000000')
-    bin_inst[0:6] = instr_info[OPCODE]
-    bin_inst[6:11] = int(r1)
-    bin_inst[11:16] = int(r2)
-    bin_inst[16:21] = int(r0)
-    bin_inst[21:26] = instr_info[SHIFT_AMT]
-    bin_inst[26:32] = instr_info[FUNCTION_CODE]
-    print bin_inst
+    bin_inst = assembler_dispatch[instr_info[INSTRUCTION_TYPE]](bin_inst, instr_info, instruction)
     return bin_inst
-instr_bin = assemble(input("Enter MIPS instruction:"))
+
+def assembler_r_type(bin_inst, instr_info, instruction):
+    tokens = re.compile("\w{2,5}\s*\$([3][0-1]|[1-2]\d|\d),\s*\$([3][0-1]|[1-2]\d|\d),\s*\$([3][0-1]|[1-2]\d|\d)").match(instruction)
+    rd = tokens.group(1)
+    rs = tokens.group(2)
+    rt = tokens.group(3)
+    bin_inst[0:6] = instr_info[OPCODE]
+    bin_inst[6:11] = int(rs)
+    bin_inst[11:16] = int(rt)
+    bin_inst[16:21] = int(rd)
+    bin_inst[21:26] = 0x00
+    bin_inst[26:32] = instr_info[FUNCTION_CODE]
+    return bin_inst
+
+def assembler_i_type(bin_inst, instr_info, instruction):
+    tokens = re.compile("\w{2,5}\s*\$([3][0-1]|[1-2]\d|\d),\s*\$([3][0-1]|[1-2]\d|\d),\s*(3[0-2][0-7][0-6][0-7]|-3[0-2][0-7][0-6][0-8]|-?1\d{4}|-?2\d{4}|-?\d{1,4})$").match(instruction)
+    rt = tokens.group(1)
+    rs = tokens.group(2)
+    imval = tokens.group(3)
+    bin_inst[0:6] = instr_info[OPCODE]
+    bin_inst[6:11] = int(rs)
+    bin_inst[11:16] = int(rt)
+    return bin_inst
+
+def assembler_j_type(bin_inst, instr_info, instruction):
+    bin_inst[0:6] = instr_info[OPCODE]
+    return bin_inst
+
+assembler_dispatch = {
+    'R': assembler_r_type,
+    'I': assembler_i_type,
+    'J': assembler_j_type
+}
+
+# instr_bin = assemble(input("Enter MIPS instruction:"))
+instr_bin = assemble("xor $1, $2, $3")
 print instr_bin
