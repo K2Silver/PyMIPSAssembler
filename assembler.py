b883bcb1ee110710eb62da925bb85bab083b44a1
Kairi Kozuma <kairikozuma@gmail.com>
2016-03-03 10:50:13 -0500

    Implement shift and shift variable instructions
    - Change variable shift instruction type from 'R' to 'SHV', because rs
    and rt have switched positions

--- assembler.py
+++ assembler.py
@@ -92,6 +92,32 @@ def assembler_mf_type(bin_inst, instr_info, instruction):
     bin_inst[26:32] = instr_info[FUNCTION_CODE]
     return bin_inst
 
+def assembler_sh_type(bin_inst, instr_info, instruction):
+    tokens = re.compile("^\s*\w{1,5}\s*\$([3][0-1]|[1-2]\d|\d),\s*\$([3][0-1]|[1-2]\d|\d),\s*([3][0-1]|[1-2]\d|\d)\s*$").match(instruction)
+    rd = tokens.group(1)
+    rt = tokens.group(2)
+    shamt = tokens.group(3)
+    bin_inst[0:6] = instr_info[OPCODE]
+    bin_inst[6:11] = 0x00
+    bin_inst[11:16] = int(rt)
+    bin_inst[16:21] = int(rd)
+    bin_inst[21:26] = int(shamt)
+    bin_inst[26:32] = instr_info[FUNCTION_CODE]
+    return bin_inst
+
+def assembler_shv_type(bin_inst, instr_info, instruction):
+    tokens = re.compile("^\s*\w{1,5}\s*\$([3][0-1]|[1-2]\d|\d),\s*\$([3][0-1]|[1-2]\d|\d),\s*\$([3][0-1]|[1-2]\d|\d)").match(instruction)
+    rd = tokens.group(1)
+    rt = tokens.group(2)
+    rs = tokens.group(3)
+    bin_inst[0:6] = instr_info[OPCODE]
+    bin_inst[6:11] = int(rs)
+    bin_inst[11:16] = int(rt)
+    bin_inst[16:21] = int(rd)
+    bin_inst[21:26] = 0x00
+    bin_inst[26:32] = instr_info[FUNCTION_CODE]
+    return bin_inst
+
 assembler_dispatch = {
     'R': assembler_r_type,
     'I': assembler_i_type,
@@ -99,9 +125,10 @@ assembler_dispatch = {
     'MEM' : assembler_mem_type,
     'B': assembler_b_type,
     'MD' : assembler_md_type,
-    'MF' : assembler_mf_type
+    'MF' : assembler_mf_type,
+    'SH' : assembler_sh_type,
+    'SHV': assembler_shv_type
     # 'JR' : assembler_jr_type,
-    # 'SH' : assembler_sh_type,
     # 'SP' : assembler_sp_type,
     # 'L' : assembler_l_type
 }
